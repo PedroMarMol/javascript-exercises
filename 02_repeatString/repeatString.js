4c74346134ceef85fe39d8b11c4f13b0a8b1adbe
Martos <ocularenginemartos@gmail.com>
2022-03-21 13:51:55 +0100

    Finish exercise 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let stringSum = "";
+    if(num < 0){ 
+        stringSum = "ERROR";
+    }   else {
+            for (let i = 0; i < num; i++){
+                stringSum += string;
+            }
+        }
+    return stringSum;
 };
 
 // Do not edit below this line
