6d6f3dc998ed6abbf0aae872c2a19c19d2c30a22
Martos <ocularenginemartos@gmail.com>
2022-03-23 12:25:56 +0100

    finish execise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(n1, n2) {
+    for (let i = n1; i <= n2; i++){
+        let sum = 0;
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
