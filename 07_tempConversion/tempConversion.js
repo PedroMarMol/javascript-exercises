2700e6b71c40308b4d3e1968b36ad33279545e80
Martos <ocularenginemartos@gmail.com>
2022-03-23 13:23:27 +0100

    finish ex_07 (tempConversion)

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(tempInFahrenheit) {
+ return Math.round((tempInFahrenheit - 32) * (5/9) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(tempInCelsius) {
+ return Math.round((tempInCelsius * 9/5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
