dd5680a77d9a856af65e020064e6516b3b7073e6
Martos <ocularenginemartos@gmail.com>
2022-03-23 12:59:17 +0100

    Simplify code into one line only

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,9 +1,5 @@
 const leapYears = function(givenYear) {
-    if ((givenYear%100 == 0 && givenYear%400 == 0) && givenYear%4 == 0){
-        return true;
-    }   else if (givenYear%4 == 0 && givenYear%100 != 0){
-        return true;
-    } else return false;
+    return (givenYear%4 === 0 && (givenYear%100 !== 0 || givenYear%400 === 0));
 };
 
 // Do not edit below this line
