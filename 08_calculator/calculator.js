95e86a25bd232c92776c12e710b6c8542133ae55
PedroMarMol <martosmoleropedro@gmail.com>
2022-04-12 14:04:13 +0200

    finish ex 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,20 @@
-const add = function() {
-	
+const add = (a,b) => a+b;
+
+const subtract = (a,b) => a-b;
+
+const sum = array => array.reduce((a,b) => a+b, 0);
+
+const multiply = array => (array.length ? array.reduce((a,b) => a*b) : 0);
+
+const power = (a,b) =>  Math.pow(a,b);
+
+const factorial = function(number) {
+	var factorialTotal = 1;
+  for (var i=number; i>0; i--){
+    factorialTotal *= i;
+  }
+  return factorialTotal;
 };
-
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
-
 // Do not edit below this line
 module.exports = {
   add,
